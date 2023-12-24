39b9940d4da6165c00dfea3247d3c8e41664ce94
kayque andrade <kayque.andrade@zigpay.com.br>
2023-12-24 14:50:17 -0300

    init

---
+++ app/src/test/java/com/example/comics/CoroutinesTestRule.kt
@@ -0,0 +1,24 @@
+package com.example.comics
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+@ExperimentalCoroutinesApi
+class CoroutinesTestRule : TestRule {
+
+    private val dispatcher = StandardTestDispatcher()
+    private val testScope = TestScope(dispatcher)
+
+    override fun apply(base: Statement, description: Description): Statement = object : Statement() {
+        override fun evaluate() {
+            Dispatchers.setMain(dispatcher)
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Desafio Tecnico Android
+
+## Este teste tem como objetivo solucionar problemas no codigo do projeto que esta gerando bugs na aplicacao
+
+### Problemas encontrados que necessitam de correcao
+> - Ao atualizar os itens da lista o app esta crashando
+
+### Necessario para iniciar a aplicacao
+> Android Studio versao giraffe
+
+### Recursos que podem ser utilizados
+> O projeto ja contem todas as ferramentas necessarias, mas tem total liberdade para adicionar lib´s externas para auxiliar na solucao
